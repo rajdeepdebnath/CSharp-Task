cc6687c63c92e31130b5539b83f3c3025735b8e8
Rajdeep Debnath <debnath.rajdeep@gmail.com>
2020-02-21 02:25:29 +0530

    Task.Delay checked in

--- Program.cs
+++ Program.cs
@@ -5,14 +5,28 @@ namespace CSharpTask
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            var t = new Task(Test);//Action, creationOptions, cancellationtoken
-            var t = Task.Factory.StartNew(Test);//Action, creationOptions, cancellationtoken, taskscheduler
-            var t = Task.Run(Test);//Action, cancellationtoken
+
+
+            //Console.WriteLine("step 1");
+
+            //var t1 = Task.Delay(2000);
+
+            //await t1;
+
+            //Console.WriteLine(t1.IsCompleted);
+
+            //Console.WriteLine("step 2");
+
+
+            //var t = new Task(Test);//Action, creationOptions, cancellationtoken
+            //var t = Task.Factory.StartNew(Test);//Action, creationOptions, cancellationtoken, taskscheduler
+            //var t = Task.Run(Test);//Action, cancellationtoken
 
             Console.WriteLine("done..");
         }
+
 
         static void Test()
         {
